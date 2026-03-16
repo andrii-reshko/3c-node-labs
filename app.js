dd62c114f75353eed2b2740dd3fc92e8f86e68ab
andrii-reshko <reshko.andrii@student.uzhnu.edu.ua>
2026-03-16 22:34:22 +0200

    upd: replace logger

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 import Fastify from 'fastify';
 import env from './plugins/env.js';
 import router from './http/routes/index.js';
-import { logger } from './utils/logger.js';
+
+// eslint-disable-next-line no-process-env
+const isDev = process.env.NODE_ENV === 'development';
 
 const fastify = Fastify({
-  logger: true,
+  logger: isDev
+    ? {
+        level: 'info',
+        transport: {
+          target: 'pino-pretty',
+        },
+      }
+    : {
+        level: 'error',
+      },
 });
 
 await fastify.register(env);
@@ -12,44 +23,39 @@ fastify.register(router);
 
 // Log server closure
 fastify.addHook('onClose', (instance, done) => {
-  logger.info('Server closed');
+  instance.log.info('Server closed');
   done();
 });
-
-// Task 2 – linter must prevent access process.env directly
-// const env = process.env.NODE_ENV;
-// console.log(env);
 
 fastify.listen(
   {
     port: fastify.config.PORT,
     host: fastify.config.HOSTNAME,
   },
-  (err, address) => {
+  (err) => {
     if (err) {
       fastify.log.error(err);
       process.exit(1);
     }
-    logger.info(`Server running at ${address}`);
   },
 );
 
 // Graceful Shutdown
 const gracefulShutdown = (signal) => {
-  logger.info(`${signal} received. shutting down gracefully`);
+  fastify.log.info(`${signal} received. shutting down gracefully`);
   fastify
     .close()
     .then(() => {
       process.exit(0);
     })
     .catch((err) => {
-      logger.error(`${err.message}`);
+      fastify.log.error(`${err.message}`);
       process.exit(1);
     });
 
   // force shutdown by timeout
   setTimeout(() => {
-    logger.error('Forcefully shutting down');
+    fastify.log.error('Forcefully shutting down');
     process.exit(1);
   }, 5000);
 };
@@ -60,12 +66,14 @@ process.on('SIGTERM', () => gracefulShutdown('SIGTERM'));
 
 // Handle global uncaught exceptions
 process.on('uncaughtException', (err, origin) => {
-  logger.error(`Uncaught at ${origin}, error: ${err.stack || err}`);
+  fastify.log.error(`Uncaught at ${origin}, error: ${err.stack || err}`);
   gracefulShutdown('uncaughtException');
 });
 
 // Handle global unhandled promise rejections
 process.on('unhandledRejection', (reason, promise) => {
-  logger.error(`rejection at ${promise}, reason: ${reason.stack || reason}`);
+  fastify.log.error(
+    `rejection at ${promise}, reason: ${reason.stack || reason}`,
+  );
   gracefulShutdown('unhandledRejection');
 });
--- utils/logger.js
+++
@@ -1,9 +0,0 @@
-// generic logger.
-const logger = {
-  info: (message) =>
-    console.log(`${new Date().toISOString()} | INFO | ${message}`),
-  error: (message) =>
-    console.error(`${new Date().toISOString()} | ERROR | ${message}`),
-};
-
-export { logger };
