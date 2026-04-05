5b265489599275016eb7bc18221bbd74ce717666
andrii-reshko <reshko.andrii@student.uzhnu.edu.ua>
2026-04-05 19:46:42 +0300

    upd: add entity defaults

--- domain/device.entity.js
+++ domain/device.entity.js
@@ -1,26 +1,28 @@
 class Device {
-  constructor(data) {
-    if (!data || typeof data !== 'object') {
-      throw new Error('Data must be an object');
-    }
-    // ID is optional because it might not be assigned yet (pre-persistence)
-    if (data.id && typeof data.id !== 'number') {
-      throw new Error('ID must be a number');
-    }
-    if (!data.device || typeof data.device !== 'string') {
+  static DEFAULTS = {
+    id: null,
+    device: '',
+    status: 'offline',
+    room: ''
+  };
+
+  constructor(data = {}) {
+    const merged = { ...Device.DEFAULTS, ...data };
+    
+    if (!merged.device || typeof merged.device !== 'string') {
       throw new Error('Device name is required and must be a string');
     }
-    if (!data.status || typeof data.status !== 'string') {
+    if (!merged.status || typeof merged.status !== 'string') {
       throw new Error('Status is required and must be a string');
     }
-    if (!data.room || typeof data.room !== 'string') {
+    if (!merged.room || typeof merged.room !== 'string') {
       throw new Error('Room is required and must be a string');
     }
 
-    this.id = data.id || null;
-    this.device = data.device;
-    this.status = data.status;
-    this.room = data.room;
+    this.id = merged.id;
+    this.device = merged.device;
+    this.status = merged.status;
+    this.room = merged.room;
   }
 }
 
