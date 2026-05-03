936eb1d4e285e81b3ab617a7f04cb7e7860ab7f5
andrii-reshko <reshko.andrii@student.uzhnu.edu.ua>
2026-05-04 00:22:42 +0300

    upd: backups

--- http/routes/v1.js
+++ http/routes/v1.js
@@ -1,3 +1,4 @@
+import * as backupController from '../controllers/backup.controller.js';
 import * as deviceController from '../controllers/device.controller.js';
 import * as healthController from '../controllers/health.controller.js';
 import * as githubController from '../controllers/github.controller.js';
@@ -94,6 +95,30 @@ async function routes(fastify) {
       );
     },
     { prefix: '/github' },
+  );
+
+  fastify.register(
+    async (route) => {
+      route.get(
+        '/:timestamp',
+        {
+          schema: {
+            tags: ['Backups'],
+            security: [{ apiKey: [] }],
+            params: {
+              type: 'object',
+              properties: {
+                timestamp: { type: 'string' },
+              },
+              required: ['timestamp'],
+            },
+          },
+          onRequest: requiresApiKey,
+        },
+        backupController.getBackup,
+      );
+    },
+    { prefix: '/backups' },
   );
 }
 
